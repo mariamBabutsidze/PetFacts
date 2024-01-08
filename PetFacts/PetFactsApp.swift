c18a94497fd267ff59fa3bf24c7510645e53bce3
Mariam Babutsidze <mariambabutsidze@gmail.com>
2024-01-08 20:16:18 +0400

    Initial Commit

---
+++ PetFacts/PetFactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PetFactsApp.swift
+//  PetFacts
+//
+//  Created by Mariam Babutsidze on 08.01.24.
+//
+
+import SwiftUI
+
+@main
+struct PetFactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
